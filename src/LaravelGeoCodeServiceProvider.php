545ad5f0076e9ce2c3be596796332cffa314e089
Diego Avelar <diego.avelar@olist.com>
2024-07-15 14:12:04 -0300

    Add publish command for configuration file
    The service provider has been updated to include a 'publishes' command. This command will allow for the geocode configuration file to be published in the application's configuration directory, thereby making it easier for users to change to their preferred geocoding service.

--- src/LaravelGeoCodeServiceProvider.php
+++ src/LaravelGeoCodeServiceProvider.php
@@ -8,10 +8,14 @@ class LaravelGeoCodeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->app->bind('geocode', function ($app) {
+        $this->app->bind('geocode', function () {
             if (config('geocode.driver') == 'opencage') {
                 return new OpenCageGeoCode();
             }
         });
+
+        $this->publishes([
+            __DIR__ . '/config.php' => config_path('geocode.php')
+        ]);
     }
 }
